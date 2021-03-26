e92717884716cddcf6ff3bc5f9a523c0ce50d553
eliane <elianekelm21@gmail.com>
2021-03-26 09:51:40 -0300

    reajuste salarial

---
+++ Mundo_1/ex013.py
@@ -0,0 +1,6 @@
+print('======== DESAFIO 013 ========')
+
+salario = float(input('Salário atual do funcionário: R$ '))
+
+print('Aumento percentual de 15%!')
+print(f'Novo salário R$ {salario * 1.15:.2f}')
