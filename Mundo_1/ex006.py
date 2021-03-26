04cfc03535f8b2acc54eb4e82f7244aa9ffefd54
eliane <elianekelm21@gmail.com>
2021-03-26 09:02:59 -0300

    calculando dobro, triplo e raiz quadrada

---
+++ Mundo_1/ex006.py
@@ -0,0 +1,8 @@
+print('======== DESAFIO 006 ========')
+
+from math import sqrt
+num = int(input('Digite um número inteiro: '))
+
+print(f'Dobro: {num * 2} \nTriplo: {num * 3} \nRaiz Quadrada: {sqrt(num):5.2f}')
+
+# Raiz quadrada também pode ser calculada pela fórmula: variável**(1/2)
