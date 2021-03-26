6c29c91fb27088549f2f595a60a3420a8d557791
eliane <elianekelm21@gmail.com>
2021-03-26 09:45:45 -0300

    calculando descontos

---
+++ Mundo_1/ex012.py
@@ -0,0 +1,6 @@
+print('======== DESAFIO 012 ========')
+
+preco = float(input('Digite o preço do produto: R$ '))
+
+print(f'Você ganhou 5% de desconto!')
+print(f'Preço a pagar: R$ {(preco * 0.95):.2f}')
