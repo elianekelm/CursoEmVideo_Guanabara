7c9e02851d513a119744bb3491b3ba0195f45436
eliane <elianekelm21@gmail.com>
2021-03-26 09:06:58 -0300

    média aritmética

---
+++ Mundo_1/ex007.py
@@ -0,0 +1,6 @@
+print('======== DESAFIO 007 ========')
+
+nota1 = float(input('Digite o valor da 1ª nota: '))
+nota2 = float(input('Digite o valor da 2ª nota: '))
+
+print(f'A média aritmética é: {(nota1 + nota2)/2}')
