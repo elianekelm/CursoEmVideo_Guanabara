c8e60429196ddfcdf90d96c28bd01752750d4a1e
eliane <elianekelm21@gmail.com>
2021-03-26 09:56:58 -0300

    conversor de temperaturas

---
+++ Mundo_1/ex014.py
@@ -0,0 +1,7 @@
+print('======== DESAFIO 014 ========')
+
+temp_cel = float(input('Informe a temperatura em ºC: '))
+temp_fah = ((9 * temp_cel) / 5) + 32
+
+print(f'A temperatura de {temp_cel}ºC corresponde a {temp_fah:.2f}ºF')
+
