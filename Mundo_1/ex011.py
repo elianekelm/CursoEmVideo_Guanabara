aca1214f6c497b98520a0dc90078b6ebb6f24faa
eliane <elianekelm21@gmail.com>
2021-03-26 09:41:23 -0300

    pintando parede

---
+++ Mundo_1/ex011.py
@@ -0,0 +1,6 @@
+print('======== DESAFIO 011 ========')
+
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+area = larg * alt
+print(f'Para pintar essa parede de {area}m² você precisará de {(area/2):.2f} litros de tinta ')
