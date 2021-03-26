570b707bb4f8acb697884973e86da92a283b9609
eliane <elianekelm21@gmail.com>
2021-03-26 09:12:59 -0300

    conversor de medidas

---
+++ Mundo_1/ex008.py
@@ -0,0 +1,5 @@
+print('======== DESAFIO 008 ========')
+
+vl_metros = int(input('Digite o valor em Metros: '))
+
+print(f'{vl_metros}m correspondem a {vl_metros * 100} centímetros e {vl_metros * 1000} milímetros')
