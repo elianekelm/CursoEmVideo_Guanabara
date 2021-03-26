77ed4c5b1de241b138cbb55ecd13814f5cbdc4b1
eliane <elianekelm21@gmail.com>
2021-03-26 10:50:40 -0300

    seno, cosseno e tangente

---
+++ Mundo_1/ex018.py
@@ -0,0 +1,11 @@
+print('======== DESAFIO 018 ========')
+
+from math import sin, cos, tan, radians
+angulo = float(input('Digite o valor do ângulo: '))
+
+print(f'Seno = {sin(radians(angulo)):.2f}')
+print(f'Cosseno = {cos(radians(angulo)):.2f}')
+print(f'Tangente = {tan(radians(angulo)):.2f}')
+
+
+
