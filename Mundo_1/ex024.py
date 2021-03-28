57f95692cb0d5038db5f2507fcf84d5dd17d027e
eliane <elianekelm21@gmail.com>
2021-03-28 20:08:00 -0300

    verificando a primeira palavra em um texto

---
+++ Mundo_1/ex024.py
@@ -0,0 +1,11 @@
+print('======== DESAFIO 024 ========')
+
+#Verifica se a cidade de nascimento contém a palavra SANTO
+# strip() elimina os espaços em branco porventura digitados
+cidade = str(input('Em que cidade você nasceu: ')).strip()
+print(cidade[:5].lower() == 'santo')
+
+
+
+
+
