ae103a3cc51fbdc753e4ce8ee344da6c4be75cc4
eliane <elianekelm21@gmail.com>
2021-04-01 12:06:58 -0300

    Analisador de Triângulos - v.1.0 update

--- Mundo_1/ex035.py
+++ Mundo_1/ex035.py
@@ -5,8 +5,8 @@ r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceiro segmento: '))
 
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('\033[34mSIM! Esses segmentos PODEM formar um triângulo!')
+    print('\033[34mEsses segmentos PODEM formar um triângulo!')
 else:
-    print('\033[31mNÃO! Esses segmentos NÃO PODEM formar um triângulo!')
+    print('\033[31mEsses segmentos NÃO PODEM formar um triângulo!')
 
 # Cada segmento tem que ser menor que a soma dos outros dois segmentos
