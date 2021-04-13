320f66c22ce211ee5dd7732a749270e558c2311f
eliane <elianekelm21@gmail.com>
2021-04-13 20:21:12 -0300

    Soma dos Pares

---
+++ Mundo_2/ex050.py
@@ -0,0 +1,11 @@
+print(f'\033[1:33m{" DESAFIO 049 " :*^40}\033[m')
+soma = 0
+cont = 0
+for i in range(1, 7):
+    num = int(input(f'Digite o {i}º número inteiro: '))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+print(f'Você digitou {cont} número(s) PAR(ES). \nA soma deles é: \033[1:34m{soma}')
+
+
