a8e5b8bcbd7b6b3462d9c1f3348386bcd16ad253
eliane <elianekelm21@gmail.com>
2021-04-13 18:15:42 -0300

    Mostrando PARES até 50

---
+++ Mundo_2/ex047.py
@@ -0,0 +1,6 @@
+print(f'\033[1:36m{" DESAFIO 047 " :*^40}\033[m')
+
+print('Pares até 50:')
+for i in range(2, 51, 2):  # pulando de 2 em 2 utiliza menos processamento
+    print(i, end=' ')
+print('\n\nEND')
