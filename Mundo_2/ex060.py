9a89006e872d6eaf41b565de77d78c7f9cc6ab25
eliane <elianekelm21@gmail.com>
2021-04-26 15:36:11 -0300

    Calculando Fatorial - n!

---
+++ Mundo_2/ex060.py
@@ -0,0 +1,5 @@
+print(f'{" FATORIAL ":=^40}')
+
+from math import factorial
+num = int(input('Digite um número para calcular seu Fatorial: '))
+print(f'O fatorial de \033[1:34m{num}!\033[m = {factorial(num)}')
